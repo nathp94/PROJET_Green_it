52735d29fd0ef4365b53827a3727d50912d08f20
ZeriiX3 <118483308+ZeriiX3@users.noreply.github.com>
2025-04-26 15:26:23 +0200

    projet de base, sans opti
    Fonctionnalité liste user supprimable par l'admin

---
+++ proj/backend/routes/user.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
+router.delete('/:id', userController.deleteUser); 
+
+module.exports = router;
